e1bd8836937b35f0a9019fd5d4d91fe014c336b9
Jupik23 <jupekkekw@gmail.com>
2025-02-22 12:13:00 +0100

    wypisanie zajec z bazy

--- backend/zajecia/models.py
+++ backend/zajecia/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class ZajecieFitness(models.Model):
+    nazwa = models.CharField(max_length=100)
+    instruktor = models.CharField(max_length=100)
+    data = models.DateTimeField()
+    liczba_miejsc = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.nazwa} - {self.data.strftime('%d-%m-%Y %H:%M')}"
--- backend/zajecia/views.py
+++ backend/zajecia/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import ZajecieFitness
 # Create your views here.
+
+def lista_zajec(request):
+    zajecia  = ZajecieFitness.objects.all()
+    return render(request, 'zajecia/lista_zajec.html', {'zajecia': zajecia})
